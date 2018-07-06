d16abb819c4d242a145c691ce0cfe83f320b6e11
fangfu <39209557@qq.com>
2018-07-06 15:12:46 +0800

    0706a

---
+++ test/zheng.py
@@ -0,0 +1,15 @@
+# ^ $ * ? + {2} {2,} {2,6} |
+# [] [^] [a-z] () .
+# \s \S \w \W
+# [\u4E00-\u9FA5] \d
+
+import re
+
+line = "python汉字2"
+str = "ppp"
+match = re.match(str, line)
+if match:
+    #print(match.group(1))
+    print("yes")
+else:
+    print("no")
